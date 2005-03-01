f663e59fbfbca0881a1e3c85d76e6234fb1bbd0e
Sebastian Riedel <kraihx@googlemail.com>
2005-03-01 12:08:59 +0000

    Import of SRI/Bundle-Catalyst-Everything-0.01 from CPAN.
    gitpan-cpan-distribution: Bundle-Catalyst-Everything
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SRI/Bundle-Catalyst-Everything-0.01.tar.gz
    gitpan-cpan-author:       SRI
    gitpan-cpan-maturity:     released

---
+++ Everything.pm
@@ -0,0 +1,60 @@
+package Bundle::Catalyst::Everything;
+
+use strict;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+Bundle::Catalyst::Everything - All Catalyst goodness
+
+=head1 SYNOPSIS
+
+C<perl -MCPAN -e 'install Bundle::Catalyst::Everything'>
+
+=head1 DESCRIPTION
+
+Installs all the Catalyst modules for you.
+
+=head1 CONTENTS
+
+Catalyst
+
+Catalyst::Plugin::Authentication::CDBI
+
+Catalyst::Plugin::Email
+
+Catalyst::Plugin::FillInForm
+
+Catalyst::Plugin::FormValidator
+
+Catalyst::Plugin::I18N
+
+Catalyst::Plugin::Session::FastMmap
+
+Catalyst::Plugin::Static
+
+Catalyst::Model::CDBI
+
+Catalyst::Model::CDBI::CRUD
+
+Catalyst::Model::Plucene
+
+Catalyst::View::REST::XML
+
+Catalyst::View::REST::YAML
+
+Catalyst::View::TT
+
+=head1 AUTHOR
+
+Sebastian Riedel, C<sri@oook.de>
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify it under
+the same terms as perl itself.
+
+=cut
+
+1;
